44112caa0d9b00db8d4315e6a6105d1b72e70dfb
cappydino <108043808+cappydino@users.noreply.github.com>
2023-12-04 16:00:04 +0000

    Done before noon, thats a PB

---
+++ day_4/main.py
@@ -0,0 +1,48 @@
+from time import sleep
+
+with open("/workspaces/Advent-of-code-2023/day_4/input.txt", "r") as file:
+    cards = file.readlines()
+
+def extractNums(numbers):
+    nums = []
+    i = 1
+    while i < len(numbers):
+        nums.append(numbers[i-1:i+1])
+        i += 3
+    
+    return nums
+
+def findMatches(card):
+    cardMatches = 0
+    parts = card.split(" | ")
+    winningNums = extractNums(parts[0].split(": ")[1])
+    ownedNums = extractNums(parts[1])
+    for winningNum in winningNums:
+        if winningNum in ownedNums:
+            cardMatches += 1
+
+    return cardMatches
+
+
+scoreSum = 0
+
+for card in cards:
+    cardScore = 0
+    cardMatches = findMatches(card)
+    for i in range(cardMatches):
+        cardScore = max(1, cardScore * 2)
+    scoreSum += cardScore
+
+
+print("part 1:", scoreSum, "points")
+
+# part 2
+
+ownedCards = [1] * len(cards)
+for cardIndex, amountOfCards in enumerate(ownedCards):
+
+    matches = findMatches(cards[cardIndex])
+    for i in range(matches):
+        ownedCards[cardIndex + 1 + i] += amountOfCards
+    
+print("part 2:", sum(ownedCards), "cards")
