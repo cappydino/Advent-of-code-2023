77d5118c7eaa4dd6f2ea397418748989bf45be7f
cappydino <108043808+cappydino@users.noreply.github.com>
2023-12-06 16:14:07 +0000

    That was easy. Thanks quadratic formula

---
+++ day_6/pt1.py
@@ -0,0 +1,32 @@
+with open("/workspaces/Advent-of-code-2023/day_6/input.txt", "r") as file:
+    inputs = file.readlines()
+
+times = inputs[0].split()[1:]
+records = inputs[1].split()[1:]
+
+output = 1
+
+for i in range(len(times)):
+    time = int(times[i])
+    record = int(records[i])
+    
+    minTime = time
+    maxTime = 0
+
+    for t in range(time):
+        d = t * (time - t)
+        if d > record:
+            print(t, d)
+            minTime = t
+            break
+
+    for t in range(time, 0, -1):
+        d = t * (time - t)
+        if d > record:
+            print(t, d)
+            maxTime = t
+            break
+    
+    output *= maxTime - minTime + 1
+
+print(output)
---
+++ day_6/pt2.py
@@ -0,0 +1,20 @@
+from math import floor, ceil, sqrt
+
+with open("/workspaces/Advent-of-code-2023/day_6/input.txt", "r") as file:
+    inputs = file.readlines()
+
+time = int(''.join(inputs[0].split()[1:]))
+record = int(''.join(inputs[1].split()[1:]))
+
+print(time, record)
+
+# d(t) = -t^2 + t*time
+# record = d(t)
+# -t^2 + t*time - record = 0
+# (time +/-sqrt( time^2 - 4*record ))/2
+# lowt =   ceil( (time - sqrt(time*time - 4*record)) / 2)
+# hight = floor( (time + sqrt(time*time - 4*record)) / 2)
+
+lowt = ceil( (time - sqrt(time*time - 4*record)) / 2)
+hight = floor( (time + sqrt(time*time - 4*record)) / 2)
+print(hight - lowt + 1)
