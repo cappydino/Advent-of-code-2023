2ff37af26cbe4442b6d6b4c217baa6e37bdd25b3
cappydino <108043808+cappydino@users.noreply.github.com>
2023-12-09 13:52:39 +0000

    I'm popping off

--- day_9/pt2.py
+++ day_9/pt2.py
@@ -2,3 +2,16 @@
 
 with open("/workspaces/Advent-of-code-2023/day_9/input.txt", "r", encoding="utf-8") as file:
     inputs = file.read().splitlines()
+
+def predictLastValue(l):
+    if type(l) == int:
+        return l
+    if all([l[i] == l[i-1] for i in range(len(l))]):
+        return l[0]
+    return l[0] - predictLastValue([l[i+1] - l[i] for i in range(len(l) - 1)])
+
+sum = 0
+for line in inputs:
+    sum += predictLastValue(list(map(int, line.split())))
+    
+print(sum)
