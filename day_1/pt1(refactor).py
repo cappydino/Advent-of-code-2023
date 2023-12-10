b159238f24694d26c6d71e36b5e97d7c55b7b43f
cappydino <108043808+cappydino@users.noreply.github.com>
2023-12-10 02:34:33 +0000

    refactor day one to actually decent code (I am learning so much)

---
+++ day_1/pt1(refactor).py
@@ -0,0 +1,11 @@
+"""pt1"""
+
+with open("/workspaces/Advent-of-code-2023/day_1/puzzle_input.txt", "r", encoding="utf-8") as file:
+    inputs = file.read().splitlines()
+
+s = 0
+for line in inputs:
+    nums = list(filter(lambda x: x.isdecimal(), [char for char in line]))
+    s += int("".join([nums[0], nums[-1]]))
+
+print(s)
---
+++ day_1/pt2(refactor).py
@@ -0,0 +1,35 @@
+"""pt2"""
+
+with open("/workspaces/Advent-of-code-2023/day_1/puzzle_input.txt", "r", encoding="utf-8") as file:
+    lines = file.read().splitlines()
+
+digits = ["one", "two", "three", "four",
+                "five", "six", "seven", "eight", "nine"]
+
+s = 0
+
+for line in lines:
+    num = ''
+    for i, char in enumerate(line):
+        if char.isdecimal():
+            num = char
+            break
+
+        if any(writtenOut := [line[i:].startswith(digit) for digit in digits]):
+            num += str(writtenOut.index(True) + 1)
+            break
+
+
+    for i, char in enumerate(line[::-1]):
+        if char.isdecimal():
+            num += char
+            break
+
+        if any(writtenOut := [line[len(line)-1-i::-1].startswith(digit[::-1]) for digit in digits]):
+            num += str(writtenOut.index(True) + 1)
+            break
+
+    s += int(num)
+
+
+print(s)
